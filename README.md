ca6e651a827e0a14a4fec7e185f596124a16c8bc
Anthony Mugendi <ngurumugz@gmail.com>
2015-08-29 19:35:26 +0300

    First Commit

---
+++ README.md
@@ -0,0 +1,65 @@
+parse-domain
+========================================================================
+**Splits an url into sub-domain, domain and top-level-domain.**
+
+Since domains are handled differently across different countries and organizations, splitting an url into sub-domain, domain and top-level-domain is not a simple regexp. **parse-domain** uses a [large list of known tlds](https://github.com/peerigon/parse-domain/blob/master/lib/build/tld.txt) (borrowed from [http://publicsuffix.org](http://publicsuffix.org/list/effective_tld_names.dat)) to recognize different parts of the domain.
+
+```javascript
+var parseDomain = require("parse-domain");
+
+expect(parseDomain("some.subdomain.example.co.uk")).to.eql({
+    subdomain: "some.subdomain",
+    domain: "example",
+    tld: "co.uk"
+});
+
+expect(parseDomain("https://user:password@example.co.uk:8080/some/path?and&query#hash")).to.eql({
+    subdomain: "",
+    domain: "example",
+    tld: "co.uk"
+});
+
+expect(parseDomain("unknown.tld.kk")).to.equal(null);
+expect(parseDomain("invalid url")).to.equal(null);
+expect(parseDomain({})).to.equal(null);
+```
+
+<br />
+
+Setup
+------------------------------------------------------------------------
+
+[![npm status](https://nodei.co/npm/parse-domain.png?downloads=true&stars=true)](https://npmjs.org/package/parse-domain)
+
+[![Build Status](https://travis-ci.org/peerigon/parse-domain.svg?branch=master)](https://travis-ci.org/peerigon/parse-domain)
+[![Dependency Status](https://david-dm.org/peerigon/parse-domain.svg)](https://david-dm.org/peerigon/parse-domain)
+[![Coverage Status](https://img.shields.io/coveralls/peerigon/parse-domain.svg)](https://coveralls.io/r/peerigon/parse-domain?branch=master)
+
+[![browser support](https://ci.testling.com/peerigon/parse-domain.png)
+](https://ci.testling.com/peerigon/parse-domain)
+
+<br />
+
+API
+------------------------------------------------------------------------
+
+### parseDomain(url: String): ParsedDomain|null
+
+Returns `null` if `url` has an unknown tld or if it's not a valid url.
+
+### ParsedDomain
+
+```
+{
+    tld: String,
+    domain: String,
+    subdomain: String
+}
+```
+
+<br />
+
+License
+------------------------------------------------------------------------
+
+Unlicense
---
+++ test/parseDomain.test.js
@@ -0,0 +1,108 @@
+"use strict";
+
+var chai = require("chai"),
+    expect = chai.expect,
+    parseDomain = require("../lib/parseDomain.js");
+
+chai.config.includeStack = true;
+
+describe("parseDomain(url)", function () {
+
+    it("should remove the protocol", function () {
+        expect(parseDomain("http://example.com")).to.eql({
+            subdomain: "",
+            domain: "example",
+            tld: "com"
+        });
+        expect(parseDomain("https://example.com")).to.eql({
+            subdomain: "",
+            domain: "example",
+            tld: "com"
+        });
+    });
+
+    it("should remove sub-domains", function () {
+        expect(parseDomain("www.example.com")).to.eql({
+            subdomain: "www",
+            domain: "example",
+            tld: "com"
+        });
+        expect(parseDomain("www.some.other.subdomain.example.com")).to.eql({
+            subdomain: "www.some.other.subdomain",
+            domain: "example",
+            tld: "com"
+        });
+    });
+
+    it("should remove the path", function () {
+        expect(parseDomain("example.com/some/path?and&query")).to.eql({
+            subdomain: "",
+            domain: "example",
+            tld: "com"
+        });
+        expect(parseDomain("example.com/")).to.eql({
+            subdomain: "",
+            domain: "example",
+            tld: "com"
+        });
+    });
+
+    it("should remove the port", function () {
+        expect(parseDomain("example.com:8080")).to.eql({
+            subdomain: "",
+            domain: "example",
+            tld: "com"
+        });
+    });
+
+    it("should remove the authentication", function () {
+        expect(parseDomain("user:password@example.com")).to.eql({
+            subdomain: "",
+            domain: "example",
+            tld: "com"
+        });
+    });
+
+    it("should also work with three-level domains like .co.uk", function () {
+        expect(parseDomain("www.example.co.uk")).to.eql({
+            subdomain: "www",
+            domain: "example",
+            tld: "co.uk"
+        });
+    });
+
+    it("should work when all url parts are present", function () {
+        expect(parseDomain("https://user@www.some.other.subdomain.example.co.uk:8080/some/path?and&query#hash")).to.eql({
+            subdomain: "www.some.other.subdomain",
+            domain: "example",
+            tld: "co.uk"
+        });
+    });
+
+    it("should also work with the minimum", function () {
+        expect(parseDomain("example.com")).to.eql({
+            subdomain: "",
+            domain: "example",
+            tld: "com"
+        });
+    });
+
+    it("should return null if the given url contains an unsupported top-level domain", function () {
+        expect(parseDomain("example.kk")).to.equal(null);
+    });
+
+    it("should return null if the given value is not a string", function () {
+        expect(parseDomain(undefined)).to.equal(null);
+        expect(parseDomain({})).to.equal(null);
+        expect(parseDomain("")).to.equal(null);
+    });
+
+    it("should work with domains that could match multiple tlds", function() {
+        expect(parseDomain("http://hello.de.ibm.com")).to.eql({
+            subdomain: "hello.de",
+            domain: "ibm",
+            tld: "com"
+        });
+    });
+
+});
